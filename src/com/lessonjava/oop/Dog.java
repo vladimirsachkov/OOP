1e19ce8536da7109a22a6bdd068ff2c707823fbe
Vladimir <alexanderredar1@gmail.com>
2021-06-03 18:00:21 +0300

    1 Класс Dog 1) Добавлен атрибут size, отвечающий за размер собаки. 2) Добавлен статический атрибут класса dogsCount. 3) Добавлен методы, отвечающие за колличество собак, за поведение и виды лая. 2.Класс Main. 1) Присвоен размер собакам. 2) Добавлены укусы. 3) Добавлен подсчёт кооличества собак.

--- src/com/lessonjava/oop/Dog.java
+++ src/com/lessonjava/oop/Dog.java
@@ -1,10 +1,36 @@
 package com.lessonjava.oop;
 
 public class Dog {
+    private static int dogsCount;
+
     private int paws;
     private int tail;
     private String name;
     private String breed;
+    private String size;
+
+    public Dog() {
+        dogsCount++;
+        System.out.println("Dog's count is " + dogsCount);
+    }
+
+    public static int getDogsCount(){
+        return dogsCount;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if(size.equalsIgnoreCase("Big") ||
+                size.equalsIgnoreCase("Average") ||
+                size.equalsIgnoreCase("Small")) {
+            this.size = size;
+        } else {
+            System.out.println("Size should be one of these: Big, Average or Small");
+        }
+    }
 
     public int getPaws() {
         return paws;
@@ -25,11 +51,11 @@ public class Dog {
     }
 
     public void setTail(int tail) {
-        if (paws == 1) {
-            this.tail = paws;
+        if (tail == 1) {
+            this.tail = tail;
         } else {
             this.tail = 1;
-            System.out.println("User tried to assign."+tail+" tail for a dog");
+            System.out.println("User tried to assign. "+tail+" tail for a dog");
             System.out.println("Correct number is 1");
         }
     }
@@ -48,5 +74,23 @@ public class Dog {
 
     public void setBreed(String breed) {
         this.breed = breed;
+    }
+
+    public void bark() {
+        if(size.equalsIgnoreCase("Big")) {
+            System.out.println("Wof - Wof");
+        } else if (size.equalsIgnoreCase("Average")) {
+            System.out.println("Raf - Raf");
+        } else if (size.equalsIgnoreCase("Small")) {
+            System.out.println("Tiaf - tiaf");
+        }
+    }
+
+    public void bite() {
+        if (dogsCount>2) {
+            System.out.println("Dogs are biting you");
+        } else {
+            bark();
+        }
     }
 }
--- src/com/lessonjava/oop/Main.java
+++ src/com/lessonjava/oop/Main.java
@@ -5,20 +5,30 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Dog's count "+Dog.getDogsCount());
+
         Dog lab = new Dog();
-        lab.setPaws(5);
-        lab.setTail(2);
+        lab.setPaws(4);
+        lab.setTail(1);
         lab.setName("Charley");
         lab.setBreed("Lab");
+        lab.setSize("Average");
+        lab.bite();
 
         Dog sheppard = new Dog();
         sheppard.setPaws(4);
         sheppard.setTail(1);
         sheppard.setName("Mike");
         sheppard.setBreed("Sheppard");
+        sheppard.setSize("Big");
+        sheppard.bite();
 
-        System.out.println("Lab's name is "+lab.getName());
-        System.out.println("Sheppard's name is" +sheppard.getName());
-        System.out.println("Lab has "+lab.getPaws()+" paws");
+        Dog doberman = new Dog();
+        doberman.setPaws(4);
+        doberman.setTail(1);
+        doberman.setName("Jake");
+        doberman.setBreed("Doberman");
+        sheppard.bite();
     }
 }
