7b3670849a957d46d9eaaa6f8118801fb9b56ca7
Vladimir <alexanderredar1@gmail.com>
2021-07-13 16:00:15 +0300

    Темы:
    1. Рассматрены модификаторы доступа.
    
    2. Рассмотрены константы и перечисления ENUM.
    
    Добавлено:
    
    1) Класс enum SIZE cо следующим перечеслением VERY_SMALL("XS"), SMALL("S"), AVERAGE("M"), BIG("L"),VERY_BIG("XL");
    
    Изменения
    
    В классе Dog:
    1) Атрибуты paws и tail теперь константы и приведены в верхний регистр согласно конвенции.
    2) Убраны методы getPaws, setPaws, getTail, setTail.
    3) В методе setSize изменён тип входного параметра на Size.
    4) В методе bark условия if изменены на Case.
    
    В классе Main.java у экземпляров класса Dog: lab, sheppard, doberman добавлены параметры com.javalesson.oop.Size.AVERAGE, com.javalesson.oop.Size.BIG и com.javalesson.oop.Size.BIG.

--- src/com/javalesson/oop/Main.java
+++ src/com/javalesson/oop/Main.java
@@ -1,34 +1,29 @@
-package com.lessonjava.oop;
+package com.javalesson.oop;
 
-import java.util.Arrays;
+import com.javalesson.oop.Dog;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Dog's count "+Dog.getDogsCount());
+        System.out.println("Dog's count "+ Dog.getDogsCount());
 
         Dog lab = new Dog();
-        lab.setPaws(4);
-        lab.setTail(1);
         lab.setName("Charley");
         lab.setBreed("Lab");
-        lab.setSize("Average");
+        lab.setSize(com.javalesson.oop.Size.AVERAGE);
         lab.bite();
 
         Dog sheppard = new Dog();
-        sheppard.setPaws(4);
-        sheppard.setTail(1);
         sheppard.setName("Mike");
         sheppard.setBreed("Sheppard");
-        sheppard.setSize("Big");
+        sheppard.setSize(com.javalesson.oop.Size.BIG);
         sheppard.bite();
 
         Dog doberman = new Dog();
-        doberman.setPaws(4);
-        doberman.setTail(1);
         doberman.setName("Jake");
         doberman.setBreed("Doberman");
+        doberman.setSize(com.javalesson.oop.Size.BIG);
         sheppard.bite();
     }
 }
---
+++ src/com/javalesson/oop/Size.java
@@ -0,0 +1,20 @@
+package com.javalesson.oop;
+
+public enum Size {
+
+    VERY_SMALL("XS"),
+    SMALL("S"),
+    AVERAGE("M"),
+    BIG("L"),
+    VERY_BIG("XL");
+
+    Size (String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    private String abbreviation;
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+}
